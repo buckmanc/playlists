df5f8cdf8423a8fd0af7ab72809746bd6f402ffb
Charles Buckman <buckmanc@gmail.com>
2024-10-01 21:29:00 -0500

    regular updates

--- scripts/generate_broad_art.sh
+++ scripts/generate_broad_art.sh
@@ -5,6 +5,8 @@ spot --playlist-name *avgc* --image-add-text --image-add-photo --image-nerd-font
 spot --playlist-name *mvgc* --image-add-text --image-add-photo --image-nerd-font-glyph '*guitar*elec*' --image-text-alignment center --image-font '*nfm*'
 spot --playlist-name liked* --image-add-text --image-add-photo --image-nerd-font-glyph '*fa*thumbsup*' --image-text-alignment center --image-font '*nfm*'
 spot --playlist-name full\ discog* --image-add-text --image-add-photo --image-nerd-font-glyph '*folder_music*' --image-text-alignment center --image-font '*nfm*'
+spot --playlist-name 'ultimate video game lofi - g*' --image-add-text --image-add-photo --image-nerd-font-glyph '*game_boy*' --image-text-alignment center --image-font '*nfm*'
+spot --playlist-name 'ultimate video game lofi - n*' --image-add-text --image-add-photo --image-nerd-font-glyph '*game_boy*' --image-text-alignment center --image-font '*nfm*'
 
 spot --playlist-name *pvgc* --image-add-text
 spot --playlist-name *hvgc* --image-add-text
@@ -12,7 +14,10 @@ spot --playlist-name *avgc* --image-add-text
 spot --playlist-name *mvgc* --image-add-text
 spot --playlist-name liked* --image-add-text
 spot --playlist-name full\ discog* --image-add-text
+spot --playlist-name 'ultimate video game lofi - g*' --image-add-text
+spot --playlist-name 'ultimate video game lofi - n*' --image-add-text
 
 # spot --playlist-name '*piano video game covers*' --image-add-text --image-add-photo --image-nerd-font-glyph '*piano*' --image-text-alignment center --image-font '*nfm*'
-spot --playlist-name '*harp video game covers*' --image-add-text --image-add-photo --image-nerd-font-glyph '*treble*' --image-text-alignment center --image-font '*nfm*'
+# spot --playlist-name '*harp video game covers*' --image-add-text --image-add-photo --image-nerd-font-glyph '*treble*' --image-text-alignment center --image-font '*nfm*'
+spot --playlist-name 'ultimate video game lofi' --image-add-text --image-add-photo --image-nerd-font-glyph '*game_boy*' --image-text-alignment center --image-font '*nfm*'
 
